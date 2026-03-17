4c689b769323e04173dca581636e09d187df593e
Roee Shoshani <roysho10@gmail.com>
2026-03-17 22:25:09 +0200

    impl Display for error type

--- src/error.rs
+++ src/error.rs
@@ -1,2 +1,7 @@
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 pub struct IndexTooBigError;
+impl core::fmt::Display for IndexTooBigError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "index too big")
+    }
+}
