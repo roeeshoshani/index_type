465e7bba1034a99644a370176228783cf0f44e55
Roee Shoshani <roysho10@gmail.com>
2026-03-17 02:47:28 +0200

    impl index type for all uint types

--- src/base_index_types.rs
+++ src/base_index_types.rs
@@ -28,3 +28,48 @@ unsafe impl IndexType for usize {
         unsafe { self.unchecked_sub(rhs) }
     }
 }
+
+macro_rules! impl_for_uint_type {
+    {$t: ty} => {
+        unsafe impl IndexType for $t {
+            const ZERO: Self = 0;
+
+            #[inline(always)]
+            fn try_from_index(index: usize) -> Result<Self, IndexTooBigError> {
+                Self::try_from(index).map_err(|_| IndexTooBigError)
+            }
+
+            #[inline(always)]
+            unsafe fn from_index_unchecked(index: usize) -> Self {
+                index as Self
+            }
+
+            #[inline(always)]
+            fn to_index(self) -> usize {
+                const _: () = if <$t>::BITS > usize::BITS {
+                    panic!()
+                };
+                self as usize
+            }
+
+            #[inline(always)]
+            unsafe fn unchecked_add_usize(self, rhs: usize) -> Self {
+                unsafe { self.unchecked_add(rhs as $t) }
+            }
+
+            #[inline(always)]
+            unsafe fn unchecked_sub(self, rhs: Self) -> Self {
+                unsafe { self.unchecked_sub(rhs) }
+            }
+        }
+    };
+}
+
+impl_for_uint_type! {u8}
+impl_for_uint_type! {u16}
+
+#[cfg(any(target_pointer_width = "32", target_pointer_width = "64"))]
+impl_for_uint_type! {u32}
+
+#[cfg(target_pointer_width = "64")]
+impl_for_uint_type! {u64}
