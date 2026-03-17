9959cb648891827d7349711c0b4ad6cb75d4b94e
Roee Shoshani <roysho10@gmail.com>
2026-03-17 03:30:13 +0200

    add declarations of functions for typed vec

--- src/typed_vec.rs
+++ src/typed_vec.rs
@@ -55,8 +55,129 @@ impl<I: IndexType, T> TypedVec<I, T> {
         })?
     }
 
-    pub fn as_mut_ptr(&mut self) -> *mut T {
+    pub const fn as_mut_ptr(&mut self) -> *mut T {
         self.ptr.as_ptr()
+    }
+
+    pub const fn capacity(&self) -> usize {
+        todo!()
+    }
+    pub fn reserve(&mut self, additional: usize) {
+        todo!()
+    }
+    pub fn reserve_exact(&mut self, additional: usize) {
+        todo!()
+    }
+    pub fn try_reserve(&mut self, additional: usize) -> Result<(), TryReserveError> {
+        todo!()
+    }
+    pub fn try_reserve_exact(&mut self, additional: usize) -> Result<(), TryReserveError> {
+        todo!()
+    }
+    pub fn shrink_to_fit(&mut self) {
+        todo!()
+    }
+    pub fn shrink_to(&mut self, min_capacity: usize) {
+        todo!()
+    }
+    pub fn into_boxed_slice(mut self) -> Box<[T], A> {
+        todo!()
+    }
+    pub fn truncate(&mut self, len: usize) {
+        todo!()
+    }
+    pub const fn as_slice(&self) -> &[T] {
+        todo!()
+    }
+    pub const fn as_mut_slice(&mut self) -> &mut [T] {
+        todo!()
+    }
+    pub const fn as_ptr(&self) -> *const T {
+        todo!()
+    }
+    pub const fn as_non_null(&mut self) -> NonNull<T> {
+        todo!()
+    }
+    pub fn allocator(&self) -> &A {
+        todo!()
+    }
+    pub unsafe fn set_len(&mut self, new_len: usize) {
+        todo!()
+    }
+    pub fn swap_remove(&mut self, index: usize) -> T {
+        todo!()
+    }
+    pub fn insert(&mut self, index: usize, element: T) {
+        todo!()
+    }
+    pub fn insert_mut(&mut self, index: usize, element: T) -> &mut T {
+        todo!()
+    }
+    pub fn remove(&mut self, index: usize) -> T {
+        todo!()
+    }
+    pub fn try_remove(&mut self, index: usize) -> Option<T> {
+        todo!()
+    }
+    pub fn retain<F>(&mut self, mut f: F) {
+        todo!()
+    }
+    pub fn retain_mut<F>(&mut self, mut f: F) {
+        todo!()
+    }
+    pub fn dedup_by_key<F, K>(&mut self, mut key: F) {
+        todo!()
+    }
+    pub fn dedup_by<F>(&mut self, mut same_bucket: F) {
+        todo!()
+    }
+    pub fn push_within_capacity(&mut self, value: T) -> Result<&mut T, T> {
+        todo!()
+    }
+    pub fn push_mut(&mut self, value: T) -> &mut T {
+        todo!()
+    }
+    pub fn pop(&mut self) -> Option<T> {
+        todo!()
+    }
+    pub fn pop_if(&mut self, predicate: impl FnOnce(&mut T) -> bool) -> Option<T> {
+        todo!()
+    }
+    pub fn peek_mut(&mut self) -> Option<PeekMut<'_, T, A>> {
+        todo!()
+    }
+    pub fn drain<R>(&mut self, range: R) -> Drain<'_, T, A> {
+        todo!()
+    }
+    pub fn clear(&mut self) {
+        todo!()
+    }
+    pub const fn len(&self) -> usize {
+        todo!()
+    }
+    pub const fn is_empty(&self) -> bool {
+        todo!()
+    }
+    pub fn split_off(&mut self, at: usize) -> Self {
+        todo!()
+    }
+    pub fn resize_with<F>(&mut self, new_len: usize, f: F) {
+        todo!()
+    }
+    pub fn leak<'a>(self) -> &'a mut [T] {
+        todo!()
+    }
+    pub fn spare_capacity_mut(&mut self) -> &mut [MaybeUninit<T>] {
+        todo!()
+    }
+    pub fn split_at_spare_mut(&mut self) -> (&mut [T], &mut [MaybeUninit<T>]) {
+        todo!()
+    }
+    pub fn into_chunks<const N: usize>(mut self) -> Vec<[T; N], A> {
+        todo!()
+    }
+    pub fn recycle<U>(mut self) -> Vec<U, A> {
+        todo!()
     }
 }
 impl<I: IndexType, T> Drop for TypedVec<I, T> {
